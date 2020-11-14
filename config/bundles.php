3f273febe6acf88aa4d0fff9e93e186290b5b613
Michał Tyc <mich.tyc@gmail.com>
2020-11-15 00:07:57 +0100

    adds docker-compose and nelmio cors

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
